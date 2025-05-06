66eb056f4f8ffffc5f74a99ce9e682051cfef890
leobragaa <leombraga11@gmail.com>
2025-05-06 17:37:28 -0400

    Atualizando as listagens

--- NetBeansProjects/sistemaLanchonete/src/implementsDao/DinheiroImplements.java
+++ NetBeansProjects/sistemaLanchonete/src/implementsDao/DinheiroImplements.java
@@ -1,8 +1,15 @@
 
 package implementsDao;
 
+import conection.Conexao;
 import dao.DinheiroDao;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.Dinheiro;
+import model.Telefone;
 
 public class DinheiroImplements implements DinheiroDao{
 
@@ -23,12 +30,31 @@ public class DinheiroImplements implements DinheiroDao{
 
     @Override
     public Dinheiro findIdForDinheiro(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+        PreparedStatement prepareStatement = Conexao.getConnection()
+            .prepareStatement("SELECT * FROM telefone WHERE cliente = ?");
+        prepareStatement.setString(5, cliente_id);
+        ResultSet resultSet = prepareStatement.getResultSet();
+
+        if (resultSet.next()) {
+            Telefone telefone = new Telefone();''''''''''''''
+            
+            telefone.setId(resultSet.getInt("id"));
+            telefone.setDd(resultSet.getInt("dd"));
+            telefone.setNumero(resultSet.getInt("numero"));
+            telefone.setFuncionario(resultSet.getString("funcionario"));
+            telefone.setCliente_id(resultSet.getString("cliente"));
+            
+            return telefone;
+        }
+    } catch (SQLException ex) {
+        Logger.getLogger(TelefoneImplements.class.getName()).log(Level.SEVERE, "Erro ao buscar telefone por Cliente", ex);
+    }
+        return null;
     }
 
     @Override
     public Dinheiro findValorEntregaForDinheiro(Double valorEntrega_id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
 }
