f284f05f6787c69606c37f505f9fa71d6858d0e4
leobragaa <leombraga11@gmail.com>
2025-05-20 14:51:53 -0400

    Atualizando a conexao com o banco de dados PostgreSQL

--- NetBeansProjects/sistemaLanchonete/src/conection/Conexao.java
+++ NetBeansProjects/sistemaLanchonete/src/conection/Conexao.java
@@ -1,11 +1,32 @@
 
 package conection;
+
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Conexao {
     private static Connection con;
+    private static Conexao obj;
     
-    public static Connection getConnection(){
+    //Conexao com o banco de dados PostgreSQL
+    private final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private final String user = "postgres";
+    private final String password = "1234";
+
+    private Conexao() {
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            con.setAutoCommit(true);
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, "Erro na Conexao com o Banco de Dados", ex);
+        }
+    }
+    public static Connection getConexao(){
+        if (obj==null){
+            obj = new Conexao();
+        }
         return con;
     }
 }
+
